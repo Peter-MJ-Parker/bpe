aaaf22fb6270af71d67c4bac57e556ecd16b4728
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-09-19 19:41:26 -0500

    speed up bpe

--- src/bpe.js
+++ src/bpe.js
@@ -1,112 +1,211 @@
-import fs from 'node:fs'
+import fs from 'node:fs';
 
-function* pairwise(arr) {
-   for (let i = 0; i < arr.length-1; i++) {
-       yield arr[i]+"%"+arr[i+1]
-   }
-}
-function frequencies (iter) {
-    const map =new Map()
-    for (const s of iter) {
-        const frq = (map.get(s) ?? 0) + 1
-        map.set(s, frq)
-    }
-    return map
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+    this.value = null;
+  }
 }
 
-function merge(tokens, best) {
-    const newtokens = []    
-    let i = 0;
-    let skip = false
-    while(i < tokens.length) {
-        if(skip) {
-            skip = false
-            i+=1
-            continue
-        }
-        if(i < tokens.length - 1 && (tokens[i] + tokens[i+1]) == best) {
-            newtokens.push(best)
-            skip = true
-        } else {
-            newtokens.push(tokens[i])
-        }
-        i+=1
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word, value) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children[char]) {
+        node.children[char] = new TrieNode();
+      }
+      node = node.children[char];
     }
-    return newtokens
+    node.isEndOfWord = true;
+    node.value = value;
+  }
+
+  longestPrefixMatch(word) {
+    let node = this.root;
+    let lastMatch = null;
+    let currentPrefix = '';
+
+    for (const char of word) {
+      if (!node.children[char]) break;
+      currentPrefix += char;
+      node = node.children[char];
+      if (node.isEndOfWord) {
+        lastMatch = currentPrefix;
+      }
+    }
+
+    return lastMatch;
+  }
 }
 
 class BPE {
-    constructor( model_name, verbose=false,) {
-        this.model_name=model_name
-        this.verbose = verbose
-        this.trained=false
-        this.model= this._load_vocabulary()
+  constructor(model_name, verbose = false) {
+    this.model_name = model_name;
+    this.verbose = verbose;
+    this.trained = false;
+    this.model = this._load_vocabulary();
+    this.trie = new Trie();
+    for (const [key, value] of Object.entries(this.model)) {
+      this.trie.insert(key, value);
     }
-    _load_vocabulary() {
-        if (fs.existsSync(this.weights_name)) {
-            this.trained = true
-            return new Map(JSON.parse(fs.readFileSync(this.weights_name, { encoding: 'utf8' })).entries())
-        }else{
-            this.trained = false 
-            return new Map()
-        }
-    }
-    train(_corpus, save = false, k = 500) {
-        if(typeof(_corpus) != 'string') {
-            throw Error("Cannot train nonstring")
-        }
-        let tokens = [..._corpus.replaceAll("%", "")]
-        let i = 0 
+  }
 
-        while(i < k) {
-            const pairedtokens = pairwise(tokens)
-            const pairFreqs = Array.from(frequencies(pairedtokens).entries())
+  _load_vocabulary() {
+    if (fs.existsSync(this.model_name)) {
+      this.trained = true;
+      return JSON.parse(fs.readFileSync(this.model_name, { encoding: 'utf8' }));
+    } else {
+      this.trained = false;
+      return {};
+    }
+  }
 
-            pairFreqs.sort(([, v0], [, v1]) => v1 - v0)
-            if(!pairFreqs.length) {
-                break
-            }
-            const [best, freq] = pairFreqs[0]
-            if (freq == 1) {
-                break
-            }
-            const [lhs, rhs] = best.split("%")
-            const decodedPair = lhs+rhs
-            tokens = merge(tokens, decodedPair)
-            this.model.set(decodedPair, this.model.size+1)
-            i++
-        }
-        if(save) {
-            fs.writeFileSync(this.model_name, JSON.stringify(Object.fromEntries(this.model)))
-        }  
-        return this.model
+  train(corpus, save = false, k = 500) {
+    if (typeof corpus !== 'string') {
+      throw new Error('Cannot train nonstring');
     }
-    tokenize(_data){
-        const tokens = [..._data.replaceAll("%", "")]
-        console.log(this.model)
-        let i = 0
-        while (i < tokens.length) {
-            let longest_match = null 
-            for (const [k, v] of this.model.entries()) {
-                console.log(k)
-              if (tokens.slice(0, k.length).join("") == k) {
-                 longest_match = k
-              }
-            }
-            if (longest_match) {
-                tokens.splice(i, longest_match.length, longest_match)
-            }
-            i += 1
-        }
-        return tokens
+
+    let tokens = corpus.replace(/%/g, '').split('');
+    const pairFreqs = {};
+    const tokenPairs = [];
+
+    for (let i = 0; i < tokens.length - 1; i++) {
+      const pair = tokens[i] + tokens[i + 1];
+      pairFreqs[pair] = (pairFreqs[pair] || 0) + 1;
+      tokenPairs.push([i, i + 1]);
     }
+
+    const heap = new MaxHeap();
+    for (const [pair, freq] of Object.entries(pairFreqs)) {
+      heap.insert([-freq, pair]);
+    }
+
+    for (let i = 0; i < k; i++) {
+      if (heap.isEmpty()) break;
+
+      const [negFreq, best] = heap.extractMax();
+      const freq = -negFreq;
+      if (freq === 1) break;
+
+      const newToken = best.replace('%', '');
+      this.model[newToken] = Object.keys(this.model).length + 1;
+      this.trie.insert(newToken, this.model[newToken]);
+
+      const newTokenPairs = [];
+      for (let j = 0; j < tokenPairs.length; j++) {
+        const [left, right] = tokenPairs[j];
+        if (tokens[left] + tokens[right] === newToken) {
+          tokens[left] = newToken;
+          tokens.splice(right, 1);
+
+          if (j > 0) {
+            const prevPair = tokens[tokenPairs[j - 1][0]] + tokens[left];
+            pairFreqs[prevPair] = (pairFreqs[prevPair] || 0) + 1;
+            heap.insert([-pairFreqs[prevPair], prevPair]);
+            newTokenPairs.push([tokenPairs[j - 1][0], left]);
+          }
+          if (j < tokenPairs.length - 1) {
+            const nextPair = tokens[left] + tokens[tokenPairs[j + 1][1]];
+            pairFreqs[nextPair] = (pairFreqs[nextPair] || 0) + 1;
+            heap.insert([-pairFreqs[nextPair], nextPair]);
+            newTokenPairs.push([left, tokenPairs[j + 1][1]]);
+          }
+        } else {
+          newTokenPairs.push([left, right]);
+        }
+      }
+      tokenPairs.length = 0;
+      tokenPairs.push(...newTokenPairs);
+    }
+
+    if (save) {
+      fs.writeFileSync(this.model_name, JSON.stringify(this.model));
+    }
+
+    return this.model;
+  }
+
+  tokenize(data) {
+    const tokens = [];
+    let i = 0;
+    while (i < data.length) {
+      const longestMatch = this.trie.longestPrefixMatch(data.slice(i));
+      if (longestMatch) {
+        tokens.push(longestMatch);
+        i += longestMatch.length;
+      } else {
+        tokens.push(data[i]);
+        i++;
+      }
+    }
+    return tokens;
+  }
 }
 
+class MaxHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-const bpe = new BPE("abc.json")
-//console.time('bpe')
-//bpe.train(fs.readFileSync("out.txt", { encoding: 'utf8' }), true)
-//console.timeEnd('bpe')
-console.log(bpe.tokenize("Moby Dick"))
+  insert(item) {
+    this.heap.push(item);
+    this.bubbleUp(this.heap.length - 1);
+  }
 
+  extractMax() {
+    if (this.isEmpty()) throw new Error('Heap is empty');
+    const max = this.heap[0];
+    const last = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.bubbleDown(0);
+    }
+    return max;
+  }
 
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[parentIndex][0] >= this.heap[index][0]) break;
+      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+      index = parentIndex;
+    }
+  }
+
+  bubbleDown(index) {
+    while (true) {
+      let largest = index;
+      const leftChild = 2 * index + 1;
+      const rightChild = 2 * index + 2;
+
+      if (leftChild < this.heap.length && this.heap[leftChild][0] > this.heap[largest][0]) {
+        largest = leftChild;
+      }
+      if (rightChild < this.heap.length && this.heap[rightChild][0] > this.heap[largest][0]) {
+        largest = rightChild;
+      }
+
+      if (largest === index) break;
+
+      [this.heap[index], this.heap[largest]] = [this.heap[largest], this.heap[index]];
+      index = largest;
+    }
+  }
+}
+
+const bpe = new BPE('abc.json');
+
+const start = performance.now();
+bpe.train(fs.readFileSync('out.txt', { encoding: 'utf8' }), true);
+const end = performance.now();
+const seconds = (end - start) / 1000;
+console.log(`BPE: ${seconds.toFixed(3)} seconds`);
